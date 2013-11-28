f6e9d80905e94fa2042f2f58721f5e60dd679277
Tilan <eric.bernelin@epfedu.fr>
2013-11-28 17:52:44 +0100

    Mise à jour des filtres

--- js/filtrespersos.js
+++ js/filtrespersos.js
@@ -6,59 +6,67 @@ var audioBuffer;
 var sourceNode;
 var analyser;
 var javascriptNode;
-var filter1;
-var filter2;
-var filter3;
+var filtre;
+//var filter2;
+//var filter3;
 
 //get the sound
 var audio = document.getElementById("song");
 
 // load the sound
-filtre_passe_bas(name);
+filtre_passe_bas(check1, check2, obj);
 
 
-function filtre_passe_bas(check1, check2, name){
+function filtre_passe_bas(check1, check2, obj){
     
     var actif = document.getElementById(check1);
-    var typefiltre = document.getElementById(check2);    
-    var range = document.getElementById(name);
+    var typefiltre = document.getElementById(check2);
 
     if(actif.checked == true)
     {
-        // setup a javascript node
+        // création d'une node javascript
         javascriptNode = context.createJavaScriptNode(2048, 1, 1);
 
-        // connect to destination, else it isn't called
+        // connection à la destination
         javascriptNode.connect(context.destination);
+        
+        // setup a analyzer
+        analyser = context.createAnalyser();
+        analyser.smoothingTimeConstant = 0.3;
+        analyser.fftSize = 512;
     
         if(typefiltre.checked == false){
-            //setsup a filter LOWPASS
-            filter1 = context.createBiquadFilter();
-            filter1.type = filter1.LOWPASS;  // In this case it's a LOWPASS filter
-            filter1.frequency.value = parseInt(range.valueOf());
-            filter1.Q = 0;
-            filter1.gain.value = 0;
+           // mise en place d'un filtre passe-bas
+            filtre = context.createBiquadFilter();
+            filtre.type = filtre.LOWPASS;  // Il s'agit d'un filtre passe-bas
+            filtre.frequency.value = 800;
+            filtre.Q = 0;
+            filtre.gain.value = 0;
         
         }
         else
         {
-            //setsup a filter HIGHPASS
-            filter1 = context.createBiquadFilter();
-            filter1.type = filter1.HIGHPASS;  // In this case it's a LOWPASS filter
-            filter1.frequency.value = parseInt(range.valueOf());
-            filter1.Q = 0;
-            filter1.gain.value = 0;
+            //mise en place d'un filtre passe-haut
+            filtre = context.createBiquadFilter();
+            filtre.type = filtre.HIGHPASS;  // Il s'agit d'un filtre passe-haut
+            filtre.frequency.value = 10000;
+            filtre.Q = 0;
+            filtre.gain.value = 0;
         }
-        // create a source node
+        // création d'une node source
         sourceNode = context.createMediaElementSource(audio);
 
         //filtre connecté
-        sourceNode.connect(filter1);
-        filter1.connect(analyser);
+        sourceNode.connect(filtre);
+        filtre.connect(analyser);
+        
+        analyser.connect(javascriptNode);
 
         //filtre connecté
-        filter1.connect(context.destination);
+        filtre.connect(context.destination);
      }   
 }
 
-
+function onError(e) {
+    console.log(e);
+}
