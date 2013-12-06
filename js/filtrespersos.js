b1ab727bf3e008c477e8e07ef26aa96c759bc936
Eric Bernelin <eric.bernelin@epfedu.fr>
2013-12-06 16:20:28 +0100

    Filtres Persos réalisés

--- js/filtrespersos.js
+++ js/filtrespersos.js
@@ -1,42 +1,57 @@
 //Javascript Document
 
-filtremono();
 
 
 //----------------Création des filtres---------------
 
-//Création d'un filtre passe-bas
-var filtre_passe_bas;
-filtre_passe_bas = context.createBiquadFilter();
-    filtre_passe_bas.type=filtre_passe_bas.LOWPASS;
-    filtre_passe_bas.frequency.value = 100;
-    filtre_passe_bas.Q = 0.8;
-    
-
 //Création d'un filtre passe-haut
-var filtre_passe_haut; 
+function setUpSpecificFilters(){
+var filtre_passe_haut;
 filtre_passe_haut = context.createBiquadFilter();
-    filtre_passe_haut.type = filtre_passe_haut.HIGHPASS;
-    filtre_passe_haut.frequency.value = 500;
-    filtre_passe_haut.Q = 0.8;
+    filtre_passe_haut.type=filtre_passe_bas.HIGHPASS;
+    filtre_passe_haut.frequency.value = 0;
+    filtre_passe_haut.Q = 0.0001;
+    tabSpecificFilters[0] = filtre_passe_haut;
+
+
+//Création d'un filtre passe-bas
+
+var filtre_passe_bas; 
+filtre_passe_bas = context.createBiquadFilter();
+    filtre_passe_bas.type = filtre_passe_bas.LOWPASS;
+    filtre_passe_bas.frequency.value = 0;
+    filtre_passe_bas.Q = 0.0001;
+    tabSpecificFilters[1] = filtre_passe_bas;
+
     
     
 //Création d'un filtre passe-bande    
+
 var filtre_passe_bande;
 filtre_passe_bande = context.createBiquadFilter();
     filtre_passe_bande.type = filtre_passe_bande.BANDPASS;
-    filtre_passe_bande.frequency.value = 700;
-    filtre_passe_bande.Q = 0.8;
-    
+    filtre_passe_bande.frequency.value = 1510;
+    filtre_passe_bande.Q = 0.0001;
+    tabSpecificFilters[2] = filtre_passe_bande;
+
     
 //Création d'un filtre coupe-bande
+
 var filtre_coupe_bande;
 filtre_coupe_bande = context.createBiquadFilter();
     filtre_coupe_bande.type = filtre_coupe_bande.NOTCH;
-    filtre_coupe_bande.frequency.value = 1000;
-    filtre_coupe_bande.Q = 0.8;
+    filtre_coupe_bande.frequency.value = 0;
+    filtre_coupe_bande.Q = 0.0001;
+    tabSpecificFilters[3] = filtre_coupe_bande;
+    
+ //Connection successive des différents filtres
+ 
+for(i=1; i<tabSpecificFilters.length; i++){
+    tabSpecificFilters[i-1].connect(tabSpecificFilters[i]);
+}
+}
 
-
+function filtre_perso(){
 //-------------Récupération des différentes Checkbox-----------
 var checkMono = document.getElementById("activePass"); //activé ou non
 var typeMono = document.getElementById("passeFilter"); //passe-haut ou passe-bas
@@ -51,60 +66,45 @@ var rangeMono = document.getElementById("rangePass"); //fréquence pour le passe-
 var rangeMultiInf = document.getElementById("rangeLowBand");//1ère fréquence du passe-bande ou coupe-bande
 var rangeMultiSup = document.getElementById("rangeHighBand");//2ème fréquence
 
-//-----------Remplissage du tableau de filtre----------------
-tabSpecificFilters[0] = filtre_passe_bas;
-tabSpecificFilters[1] = filtre_passe_haut;
-tabSpecificFilters[2] = filtre_passe_bande;
-tabSpecificFilters[3] = filtre_coupe_bande;
 
-
-
-function changeCheckBox(){
-    if(checkMono.checked == true)
-    {
-        if(typeMono.checked == false)
-        {
-            changeChannel();
+    //Case passe-haut ou passe-bas cochée
+    if(checkMono.checked === true){
+        if(typeMono.checked === false){ //passe-haut
+            tabSpecificFilters[0].frequency.value = rangeMono.value;
+            tabSpecificFilters[0].Q = Math.round(range.value/2980*10)/10;
+        }
+        else{ //passe-bas
+            tabSpecificFilters[1].frequency.value = rangeMono.value;
+            tabSpecificFilters[1].Q = Math.round(range.value/2980*10)/10;
+        }
+    }
+    else{//Case décochée
+        if(checkMono.checked !== true){
+            tabSpecificFilters[0].frequency.value = 0;
+            tabSpecificFilters[0].Q = 0.0001;
+        }else{
+            tabSpecificFilters[1].frequency.value = 0;
+            tabSpecificFilters[1].Q = 0.0001;
+        }
+    }
+    
+    //Case passe-bande ou coupe-bande cochée
+    if(checkMulti.checked === true){
+        if(typeMulti.checked === false){//passe-bande activée
+            tabSpecificFilters[2].frequency.value = (rangeMultiInf.value+rangeMultiSup.value)/2;
+            tabSpecificFilters[2].Q = Math.round(((rangeMultiInf.value+rangeMultiSup.value)/2)/2980*10)/10;
+        }else{//coupe-bande activée
+            tabSpecificFilters[3].frequency.value = (rangeMultiInf.value+rangeMultiSup.value)/2;
+            tabSpecificFilters[3].Q = Math.round(((rangeMultiInf.value+rangeMultiSup.value)/2)/2980*10)/10;
+        }
+    }else{ //Case décochée
+        if(checkMulti.checked !== true){//passe-bande désactivée
+            tabSpecificFilters[2].frequency.value = 1510;
+            tabSpecificFilters[2].Q = 0.0001;
+        }else{//coupe-bande désctivée
+            tabSpecificFilters[3].frequency.value = 1510;
+            tabSpecificFilters[3].Q = 0.0001;
         }
     }
 }
-function filtremono(){
 
-    
-        if(actif.checked == true)
-        {
-            if(typefiltre.checked == false){
-                // mise en place d'un filtre passe-haut
-                filtre = context.createBiquadFilter();
-                filtre.type = filtre.HIGHPASS;  // Il s'agit d'un filtre passe-haut
-                filtre.frequency.value = range.value+".0";
-                filtre.Q = 50;//Math.round(range.value/2980*10)/10;
-        
-            }
-            else
-            {
-                //mise en place d'un filtre passe-bas
-                filtre = context.createBiquadFilter();
-                filtre.type = filtre.LOWPASS;  // Il s'agit d'un filtre passe-bas
-                filtre.frequency.value = range.value+".0";
-                filtre.Q = 0.8;
-            }
-            
-            //filtre connecté
-            sourceNode.connect(filtre);
-            filtre.connect(analyser);
-            
-            //filtre connecté
-            filtre.connect(context.destination);
-          }
-        
-        
-}
-
-
-function changeChannel()
-function changeChannelCharacs(tabFreq)
-
-function onError(e) {
-    console.log(e);
-}
