f4f27a19de45723e086ec1d311dbfc2505f977bb
Eric Bernelin <eric.bernelin@epfedu.fr>
2013-12-14 18:06:23 +0100

    Commentaires js Raf

--- js/filtrespersos.js
+++ js/filtrespersos.js
@@ -1,121 +1,126 @@
 //Javascript Document
+//----------------Creation des filtres---------------
 
-//Creation des filtres
+function setUpSpecificFilters(){
 
-//Creation d'un filtre passe-haut
-function setUpSpecificFilters() {
-
-    var filter;
-    for (i = 0; i < 4; i++)
-    {
-        filter = context.createBiquadFilter();
-        filter.type = filter.PEAKING;
-        filter.frequency.value = 0;
-        filter.Q = 2;
-        filter.gain.value = 0;
-        tabSpecificFilters.push(filter);
-        if (i > 0)
-        {
-            tabSpecificFilters[i - 1].connect(tabSpecificFilters[i]);
-        }
-    }
+var filter;
+	for (i = 0; i < 4; i++) 
+		{ 
+			//creation des filtres
+			filter = context.createBiquadFilter();
+			filter.type = filter.PEAKING;  
+			filter.frequency.value =0;
+			filter.Q=2;
+			filter.gain.value=0;
+			tabSpecificFilters.push(filter);
+			if(i>0)
+				{
+					//connection des filtres
+					tabSpecificFilters[i-1].connect(tabSpecificFilters[i]);
+				}				
+		} 
 }
 
-function filtre_perso() {
-//Recuperation des differentes Checkbox
-    //active ou non
-    var checkMono = document.getElementById("activePass");
-    //passe-haut ou passe-bas
-    var typeMono = document.getElementById("passeFilter");
-    //active ou non
-    var checkMulti = document.getElementById("activeBand");
-    //passe-bande ou coupe-bande
-    var typeMulti = document.getElementById("bandeFilter");
+function filtre_perso(){
+//-------------Recuperation des differentes Checkbox-----------
+var checkMono = document.getElementById("activePass");
+ //active ou non
+var typeMono = document.getElementById("passeFilter");
+ //passe-haut ou passe-bas
+var checkMulti = document.getElementById("activeBand");
+ //activé ou non
+var typeMulti = document.getElementById("bandeFilter");
+ //passe-bande ou coupe-bande
 
-
-//Recuperation des champs de selection des frequences
-    //frequence pour le passe-haut ou passe-bas
-    var rangeMono = document.getElementById("rangePass");
-    //1ere frequence du passe-bande ou coupe-bande
-    var rangeMultiInf = document.getElementById("rangeLowBand");
-    //2eme frequence
-    var rangeMultiSup = document.getElementById("rangeHighBand");
+ 
+//-------------Recuperation des champs de selection des frequences-------
+var rangeMono = document.getElementById("rangePass"); 
+//frequence pour le passe-haut ou passe-bas
+var rangeMultiInf = document.getElementById("rangeLowBand");
+//1ere frequence du passe-bande ou coupe-bande
+var rangeMultiSup = document.getElementById("rangeHighBand");
+//2ème frequence
 
 
     //Case passe-haut ou passe-bas cochee
-    if (checkMono.checked == true) {
-        //passe-haut
-        if (typeMono.checked == false) {
-            tabSpecificFilters[0].type = tabSpecificFilters[0].HIGHPASS;
+    if(checkMono.checked == true){
+        if(typeMono.checked == false){
+		//passe-haut
+		//activation du passe haut
+		    tabSpecificFilters[0].type=tabSpecificFilters[0].HIGHPASS;
             tabSpecificFilters[0].frequency.value = rangeMono.value;
-            tabSpecificFilters[1].type = tabSpecificFilters[1].LOWSHELF;
-            tabSpecificFilters[1].gain = 0;
-
+			//desactivation du passe bas
+			tabSpecificFilters[1].type=tabSpecificFilters[0].LOWSHELF;
+			tabSpecificFilters[1].gain = 0;
             tabSpecificFilters[1].frequency.value = 0;
             tabSpecificFilters[1].Q = 0.0001;
-
+			
 
         }
-        //passe-bas
-        else {
-            tabSpecificFilters[1].type = tabSpecificFilters[1].LOWPASS;
+        else{ 
+		//passe-bas
+		//activation du passe bas
+			tabSpecificFilters[1].type=tabSpecificFilters[1].LOWPASS;
             tabSpecificFilters[1].frequency.value = rangeMono.value;
-
+			//desactivation du passe haut
+			tabSpecificFilters[0].type=tabSpecificFilters[0].LOWSHELF;
+			tabSpecificFilters[0].gain = 0;
             tabSpecificFilters[0].frequency.value = 0;
             tabSpecificFilters[0].Q = 0.0001;
-            tabSpecificFilters[0].type = tabSpecificFilters[0].LOWSHELF;
-            tabSpecificFilters[0].gain = 0;
+        
         }
     }
-    //Case decochee
-    else {
-
-        tabSpecificFilters[0].frequency.value = 0;
-        tabSpecificFilters[0].Q = 0.0001;
-        tabSpecificFilters[0].type = tabSpecificFilters[0].LOWSHELF;
-        tabSpecificFilters[0].gain = 0;
-        tabSpecificFilters[1].type = tabSpecificFilters[1].LOWSHELF;
-        tabSpecificFilters[1].gain = 0;
-
-        tabSpecificFilters[1].frequency.value = 0;
-        tabSpecificFilters[1].Q = 0.0001;
-
+    else{
+	//Case decochee
+	//desactivation des deux types de filtres
+			tabSpecificFilters[0].type=tabSpecificFilters[0].LOWSHELF;
+			tabSpecificFilters[0].gain = 0;
+            tabSpecificFilters[0].frequency.value = 0;
+            tabSpecificFilters[0].Q = 0.0001;
+			tabSpecificFilters[1].type=tabSpecificFilters[0].LOWSHELF;
+			tabSpecificFilters[1].gain = 0;
+            tabSpecificFilters[1].frequency.value = 0;
+            tabSpecificFilters[1].Q = 0.0001;
+        
     }
-
+    
     //Case passe-bande ou coupe-bande cochee
-    if (checkMulti.checked == true) {
-        //passe-bande activee
-        if (typeMulti.checked == false) {
-            tabSpecificFilters[2].type = tabSpecificFilters[2].BANDPASS;
-            tabSpecificFilters[2].frequency.value = (parseInt(rangeMultiInf.value) + parseInt(rangeMultiSup.value)) / 2;
-            tabSpecificFilters[2].Q = tabSpecificFilters[2].frequency.value / Math.abs(parseInt(rangeMultiSup.value) - parseInt(rangeMultiSup.value));
-            tabSpecificFilters[3].type = tabSpecificFilters[3].LOWSHELF;
+    if(checkMulti.checked == true){
+        if(typeMulti.checked == false){
+		//passe-bande activee
+			tabSpecificFilters[2].type=tabSpecificFilters[2].BANDPASS;
+            tabSpecificFilters[2].frequency.value = (parseInt(rangeMultiInf.value)+parseInt(rangeMultiSup.value))/2;
+            tabSpecificFilters[2].Q = tabSpecificFilters[2].frequency.value/Math.abs(parseInt(rangeMultiSup.value)-parseInt(rangeMultiSup.value)); 
+			//desactivation du coupe bande
+			tabSpecificFilters[3].type=tabSpecificFilters[3].LOWSHELF;
             tabSpecificFilters[3].frequency.value = 0;
             tabSpecificFilters[3].Q = 0;
-            tabSpecificFilters[3].gain = 0;
-        }
-        //coupe-bande activee
-        else {
-            tabSpecificFilters[3].type = tabSpecificFilters[3].NOTCH;
-            tabSpecificFilters[3].frequency.value = (parseInt(rangeMultiInf.value) + parseInt(rangeMultiSup.value)) / 2;
-            tabSpecificFilters[3].Q = tabSpecificFilters[3].frequency.value / Math.abs(parseInt(rangeMultiSup.value) - parseInt(rangeMultiSup.value));
-            tabSpecificFilters[2].type = tabSpecificFilters[2].LOWSHELF;
+			tabSpecificFilters[3].gain = 0;
+        }else{
+		//coupe-bande activee
+			tabSpecificFilters[3].type=tabSpecificFilters[3].NOTCH;
+            tabSpecificFilters[3].frequency.value = (parseInt(rangeMultiInf.value)+parseInt(rangeMultiSup.value))/2;
+            tabSpecificFilters[3].Q = tabSpecificFilters[3].frequency.value/Math.abs(parseInt(rangeMultiSup.value)-parseInt(rangeMultiSup.value));
+			//desactivation du passe bande
+			tabSpecificFilters[2].type=tabSpecificFilters[2].LOWSHELF;
             tabSpecificFilters[2].frequency.value = 1510;
             tabSpecificFilters[2].Q = 0;
-            tabSpecificFilters[2].gain = 0;
+			tabSpecificFilters[2].gain = 0;
         }
     }
-    //Case decochee
-    else {
-        tabSpecificFilters[2].type = tabSpecificFilters[2].LOWSHELF;
-        tabSpecificFilters[2].frequency.value = 1510;
-        tabSpecificFilters[2].Q = 0;
-        tabSpecificFilters[2].gain = 0;
+	else{ 
+	//Case decochee
+	//desactivation des deux types de filtres
 
-        tabSpecificFilters[3].type = tabSpecificFilters[3].LOWSHELF;
-        tabSpecificFilters[3].frequency.value = 0;
-        tabSpecificFilters[3].Q = 0;
-        tabSpecificFilters[3].gain = 0;
-
+			tabSpecificFilters[2].type=tabSpecificFilters[2].LOWSHELF;
+            tabSpecificFilters[2].frequency.value = 1510;
+            tabSpecificFilters[2].Q = 0;
+			tabSpecificFilters[2].gain = 0;
+			tabSpecificFilters[3].type=tabSpecificFilters[3].LOWSHELF;
+            tabSpecificFilters[3].frequency.value = 0;
+            tabSpecificFilters[3].Q = 0;
+			tabSpecificFilters[3].gain = 0;
+        
     }
 }
+
--- js/noiseFilter.js
+++ js/noiseFilter.js
@@ -1,36 +1,32 @@
-// JavaScript Document
 
+//-------------DECLARATION DES FILTRES PREDEFINIS POUR CERTAINS BRUITS IDENTIFIES-------------
 
-//DECLARATION DES FILTRES PREDEFINIS POUR CERTAINS BRUITS IDENTIFIES
-
-//Bruit GSM
+//------------------------Bruit GSM
 var GSMFilter;
 function setUpGSMFilter()
 {
-    GSMFilter = context.createBiquadFilter();
-    // dans ce cas un PEAKING filter
-    GSMFilter.type = GSMFilter.PEAKING;
-    GSMFilter.frequency.value = 240.0;
-    //compris entre 0.0001 et 1000 (Q=f0/deltaf)
+GSMFilter = context.createBiquadFilter();
+    GSMFilter.type = GSMFilter.PEAKING; 
+	// dans ce cas un PEAKING filter
+    GSMFilter.frequency.value = 240.0; 
     GSMFilter.Q = 0.8;
-    //compris entre -40 et 40
     GSMFilter.gain.value = 0;
 }
-//fonction case GSM cochee
-function noise_activeGSM_checked() {
+//--------------------Controle filtre------------------------
+function noise_activeGSM_checked(){
     //get the checkbox
-    //active, non active
-    var noiseGSM = document.getElementById("activeGSM");
-
-    // case gains cochee
-    if (noiseGSM.checked === true) {
-        GSMFilter.gain.value = -30;
-
-    }
-    //case gain decochee
-    else {
-        // ou diminuer desactive
-        console.log("Filtre GSM desactivé");
-        GSMFilter.gain.value = 0;
-    }
-}
+    var noiseGSM = document.getElementById("activeGSM"); //activé, non active
+    
+    // case gains cochee 
+    if(noiseGSM.checked === true){              
+         GSMFilter.gain.value=-30;
+           
+    }else{
+	//case gain decochee                                           
+          console.log("Filtre GSM desactivé");
+		  // ou diminuer desactive
+        
+			GSMFilter.gain.value=0;
+		
+             }  
+     }    
